eb513f897fcd0967d4f190353384f3df09ee2a3a
Amrit Gupta <amritgupta1018@gmail.com>
2022-12-25 23:20:55 +0530

    2nd commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './submodules/backend-social-1.0-entities/src/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db-socialmedia.covmrskhmzs0.ap-south-1.rds.amazonaws.com',
+      port: 5432,
+      username: 'postgres',
+      password: 'amrit1234',
+      database: 'backend-socialmedia',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
