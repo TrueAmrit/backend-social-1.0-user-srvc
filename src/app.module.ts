e649d9b87c0ddad1f715e36f010b140c4f3e5642
Amrit Gupta <amritgupta1018@gmail.com>
2022-12-28 00:04:19 +0530

    added dtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
 import { User } from './submodules/backend-social-1.0-entities/src/entities/user.entity';
 
 @Module({
@@ -16,6 +17,7 @@ import { User } from './submodules/backend-social-1.0-entities/src/entities/user
       entities: [User],
       synchronize: true,
     }),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserDto } from 'src/submodules/backend-social-1.0-dtos/src/dtos/user.dto';
 import { UserService } from './user.service';
 
-@Controller()
+@Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
-  getHello(): string {
-    return this.userService.getHello();
+  @Post()
+  async createUser(@Body() user: UserDto) {
+    try {
+      // let createUser = await this.userService.createUser(user);
+      console.log('hello', user);
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
   }
 }
